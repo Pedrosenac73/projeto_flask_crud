fb526bc4980eeb6648e7a5a3d1481cc6bc51a6cd
Pedrosenac73 <pedro.rnogueira@aluno.senac.rio>
2026-03-12 20:28:34 -0300

    Ajustes finais

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBIhBRvJfXYRA-WHLf51uBuTPFg_4-KQl4",
+  authDomain: "projetoflaskcrud-286cf.firebaseapp.com",
+  projectId: "projetoflaskcrud-286cf",
+  storageBucket: "projetoflaskcrud-286cf.firebasestorage.app",
+  messagingSenderId: "924594513183",
+  appId: "1:924594513183:web:97f9aa90fc8d639feffc2a"
+};
